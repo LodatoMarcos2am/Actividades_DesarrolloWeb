9107f76057fcbe7e18ccdff8cc1c2befb5b7f6da
Marcos Lodato <marcos.lodato@alumnos.uai.edu.ar.com>
2025-06-16 17:08:41 -0300

    Agregamos Contraseñas y validaciones varias con JS

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var formulario = document.querySelector(".Formulario_suscripcion");
 var elementos = formulario.querySelectorAll("input, select");
 var boton = document.getElementById("Boton_Submit");
-
+var errorSpan = document.getElementById("error-contraseña");
 
 function actualizarBorde(elemento) {
   if (elemento.value.trim() === "") {
@@ -37,6 +37,8 @@ elementos.forEach(function(elemento) {
 boton.addEventListener("click", function(event) {
   event.preventDefault();
   var hayVacios = false;
+  let pass1Contenido = document.getElementById("Contraseña").value;
+  let pass2Contenido = document.getElementById("Contraseña2").value;
 
   elementos.forEach(function(elemento) {
     if (elemento.value.trim() === "") {
@@ -47,8 +49,25 @@ boton.addEventListener("click", function(event) {
 
   if (hayVacios) {
     alert("Hay campos vacíos. Por favor, completalos.");
-  } else {
-    alert("Formulario válido. Podés enviarlo.");
-
+    return; 
   }
+
+  if (pass1Contenido !== pass2Contenido) {
+    errorSpan.textContent = "Las contraseñas deben ser iguales.";
+    errorSpan.style.color = "red";
+    return; 
+  } else {
+    errorSpan.textContent = "";
+  }
+
+  alert("Formulario válido, podés enviarlo.");
+  LimpiarElementos();
 });
+
+
+function LimpiarElementos()
+{
+  elementos.forEach(element => {
+    element.value = "";
+  });
+}
